ccde84ea4c68c176eb0e0ae9a23b711a60002b35
Dominik Schmidt <schmidtdominik30@gmail.com>
2019-10-02 20:56:06 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 pip install -U pip
 pip install tensorflow-gpu==2.0.0
+
+pip install kaggle
+pip install tqdm
+pip install imageio
+pip install comet_ml
+pip install matplotlib
+pip install jupyter;
